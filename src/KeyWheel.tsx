12f08e966e7942416ffde66bb6b2dfa2fe967c8a
Chet Corcos <ccorcos@gmail.com>
2017-06-25 16:01:13 -0700

    rendering but the paths are wrong :(

--- src/KeyWheel.tsx
+++ src/KeyWheel.tsx
@@ -1,18 +1,84 @@
 import * as React from "react"
 import * as _ from "lodash"
 import Component from "reactive-magic/component"
-import { Pegs, createKeyWheelGraph } from "./utils"
+import { Pegs, Scale, createKeyWheelGraph, Direction } from "./utils"
 
 interface KeyWheelProps {
   pegs: Pegs
 }
 
 class KeyWheel extends Component<KeyWheelProps> {
+  scale: Scale
   willMount() {
-    console.log(createKeyWheelGraph(this.props.pegs))
+    this.scale = createKeyWheelGraph(this.props.pegs)
+    console.log(this.scale)
   }
   view() {
-    return <div />
+    const rendered: { [id: string]: boolean } = {}
+    let components: Array<JSX.Element> = []
+    let origin = {
+      x: 700,
+      y: 300,
+    }
+    const noteRadius = 10
+    const noteSize = noteRadius * 2
+    const edgePadding = 5
+    const edgeLength = 50
+    const edgeTotalLength = edgeLength + edgePadding * 2 + noteRadius
+    const edgeWidth = 5
+    rendered[this.scale.id] = true
+    components.push(
+      <div
+        key={this.scale.id}
+        style={{
+          position: "absolute",
+          top: origin.y - noteRadius,
+          left: origin.x - noteRadius,
+          width: noteSize,
+          height: noteSize,
+          borderRadius: noteSize,
+          backgroundColor: "black",
+        }}
+      />
+    )
+
+    this.scale.edges.forEach(edge => {
+      rendered[edge.id] = true
+      console.log(edge.direction)
+      components.push(
+        <div
+          key={edge.id}
+          style={{
+            position: "absolute",
+            top: origin.y - edgeTotalLength,
+            left: origin.x - edgeWidth / 2,
+            width: edgeWidth,
+            height: edgeTotalLength,
+            transformOrigin: "center bottom",
+            transform: `rotate(${edge.direction === Direction.up
+              ? -Math.PI / 4
+              : edge.direction === Direction.right
+                ? -Math.PI * 3 / 4
+                : edge.direction === Direction.down
+                  ? Math.PI * 3 / 4
+                  : edge.direction === Direction.left ? Math.PI / 4 : 0}rad)`,
+          }}
+        >
+          <div
+            style={{
+              position: "absolute",
+              top: edgePadding,
+              left: 0,
+              width: edgeWidth,
+              height: edgeLength,
+              backgroundColor: "black",
+            }}
+          />
+        </div>
+      )
+    })
+
+    return <div>{components}</div>
   }
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import * as _ from "lodash"
 import { modPos, modMinus } from "./mod-math"
+;(window as any)["_"] = _
 
 // Pegs are the location of the 7 pegs.
 export type Pegs = Array<number>
@@ -20,7 +21,7 @@ enum Spin {
 }
 
 // Direction is the direction that the edge should be layed out on the lattice.
-enum Direction {
+export enum Direction {
   up,
   down,
   left,
@@ -40,7 +41,7 @@ class Index<Type> {
 const ScaleIndex = new Index<Scale>()
 
 // A Scale represents a set of notes
-class Scale {
+export class Scale {
   id: string
   pegs: Pegs
   edges: Array<Edge> = []
@@ -51,8 +52,16 @@ class Scale {
     ScaleIndex.set(this.id, this)
   }
 
-  findEdge(scale: Scale): Edge {
-    return this.edges.find(edge => edge.scale.id === scale.id)
+  findEdgeTo(scale: Scale): Edge {
+    return this.edges.find(edge => edge.toScale.id === scale.id)
+  }
+
+  findEdgeDirection(direction: Direction): Edge {
+    return this.edges.find(edge => edge.direction === direction)
+  }
+
+  isInPentatonic(pentatonic: Pentatonic) {
+    return _.some(this.pentatonics, p => p.id === pentatonic.id)
   }
 
   // Creates an id from a scale by creating a binary number
@@ -63,16 +72,29 @@ class Scale {
   static lookup(pegs: Pegs): Scale {
     return ScaleIndex.get(Scale.makeScaleId(pegs))
   }
+
+  static getCommonPentatonics(scales: Array<Scale>) {
+    return _.intersection(...scales.map(scale => scale.pentatonics))
+  }
+
+  static getCommonPentatonic(scales: Array<Scale>) {
+    return Scale.getCommonPentatonics(scales)[0]
+  }
 }
 
 // An edge represents a one-note substitution to another scale
-class Edge {
+export class Edge {
+  id: string
   peg: number
-  scale: Scale
+  fromScale: Scale
+  toScale: Scale
   spin: Spin
   direction?: Direction
-  constructor(peg: number, spin: Spin, scale: Scale) {
-    this.scale = scale
+  constructor(peg: number, spin: Spin, fromScale: Scale, toScale: Scale) {
+    // this id is the same for both spins!
+    this.id = bitwiseAndStrings(fromScale.id, toScale.id)
+    this.fromScale = fromScale
+    this.toScale = toScale
     this.spin = spin
     this.peg = peg
   }
@@ -83,7 +105,7 @@ const PentatonicIndex = new Index<Pentatonic>()
 function bitwiseAndStrings(str1: string, str2: string): string {
   const chars = str1.split("")
   for (let i = 0; i < str1.length; i++) {
-    chars[i] = str1[i] === "1" || str2[i] === "1" ? "1" : "0"
+    chars[i] = str1[i] === "1" && str2[i] === "1" ? "1" : "0"
   }
   return chars.join("")
 }
@@ -101,7 +123,7 @@ class Pentatonic {
   getEdgePairs(): Array<Array<Edge>> {
     return this.scales.map((scale, index) => {
       const nextScale = this.scales[modPos(index + 1, this.scales.length)]
-      return [scale.findEdge(nextScale), nextScale.findEdge(scale)]
+      return [scale.findEdgeTo(nextScale), nextScale.findEdgeTo(scale)]
     })
   }
 
@@ -116,21 +138,36 @@ class Pentatonic {
   }
 }
 
+function distance(x: number, y: number): number {
+  return Math.abs(modMinus(x, y, 12))
+}
+
 // Tweak attempts to move a peg at a certain index. If it can, it will return
 // a new set of pegs along with the spin, otherwise it returns null
 function tweak(pegs: Pegs, index: number): { pegs: Pegs; spin: Spin } {
-  const downIndex = modPos(index - 1, pegs.length)
-  const upIndex = modPos(index + 1, pegs.length)
-  const down = pegs[downIndex]
-  const up = pegs[upIndex]
+  const down = pegs[modPos(index - 1, pegs.length)]
+  const downDown = pegs[modPos(index - 2, pegs.length)]
+  const up = pegs[modPos(index + 1, pegs.length)]
+  const upUp = pegs[modPos(index + 2, pegs.length)]
   const it = pegs[index]
-  if (modMinus(up, it, 12) === 2 && modMinus(it, down, 12) === 1) {
+  // Move a note up to an empty space without creating three steps in a row
+  if (
+    distance(up, it) === 2 &&
+    distance(it, down) === 1 &&
+    distance(upUp, up) !== 1
+  ) {
     const newPegs = _.clone(pegs)
-    newPegs[index] += 1
+    newPegs[index] = modPos(newPegs[index] + 1, 12)
     return { pegs: newPegs, spin: Spin.up }
-  } else if (modMinus(up, it, 12) === 1 && modMinus(it, down, 12) === 2) {
+  }
+  // Move a note down to an empty space without creating three steps in a row
+  if (
+    distance(up, it) === 1 &&
+    distance(it, down) === 2 &&
+    distance(down, downDown) !== 1
+  ) {
     const newPegs = _.clone(pegs)
-    newPegs[index] -= 1
+    newPegs[index] = modPos(newPegs[index] - 1, 12)
     return { pegs: newPegs, spin: Spin.down }
   }
   return null
@@ -151,46 +188,80 @@ function generateGraph(pegs: Pegs): Scale {
         nextScale = generateGraph(result.pegs)
       }
       // assign the edge
-      scale.edges.push(new Edge(peg, result.spin, nextScale))
+      scale.edges.push(new Edge(peg, result.spin, scale, nextScale))
     }
   })
 
   return scale
 }
 
-function permutePaths(
-  scale: Scale,
-  parent: Scale,
-  depth: number
+function permute(
+  path: Array<Scale>,
+  next: (path: Array<Scale>, edge: Edge) => boolean
 ): Array<Array<Scale>> {
-  if (depth === 0) {
+  const scale = _.last(path)
+  const edges = scale.edges.filter(edge => next(path, edge))
+  if (edges.length === 0) {
     return []
+  } else {
+    // Breadth-First Search
+    let paths = edges.map(edge => [...path, edge.toScale])
+    const results: Array<Array<Scale>> = []
+    while (paths.length > 0) {
+      const path = paths.shift()
+      const morePaths = permute(path, next)
+      if (morePaths.length > 0) {
+        paths = paths.concat(morePaths)
+      } else {
+        results.push(path)
+      }
+    }
+    return results
   }
-  const paths = scale.edges
-    .filter(edge => (parent ? edge.scale.id !== parent.id : true))
-    .map(edge => permutePaths(edge.scale, scale, depth - 1))
-    .reduce((acc, paths) => acc.concat(paths), [])
-    .map(path => [parent, ...path])
-  return paths
+}
+
+function findLoops(scale: Scale): Array<Array<Scale>> {
+  // permute all paths of up to length 5 that don't go backwards
+  const permutations = permute([scale], (path, edge) => {
+    if (path.length === 5) {
+      return false
+    }
+    // Complete the loop
+    if (path.length === 4) {
+      return edge.toScale.id === _.first(path).id
+    }
+    // Explore all directions
+    if (path.length === 1) {
+      return true
+    }
+    // No returning back over the scale
+    if (_.nth(path, -2).id === edge.toScale.id) {
+      return false
+    }
+    return true
+  })
+  const loops = permutations
+    .filter(path => _.first(path).id === _.last(path).id)
+    .map(path => _.initial(path))
+  return loops
 }
 
 export function generatePentatonics(scale: Scale) {
   let foundNewPentatonic = false
-  console.log(permutePaths(scale, null, 4))
-  scale.pentatonics = permutePaths(scale, null, 4)
-    .filter(path => _.first(path).id === _.last(path).id)
-    .map(scales => {
-      const pentatonic = Pentatonic.lookup(scales)
+  scale.pentatonics = _.uniq(
+    findLoops(scale).map(path => {
+      const pentatonic = Pentatonic.lookup(path)
       if (pentatonic) {
         return pentatonic
       } else {
         foundNewPentatonic = true
-        return new Pentatonic(scales)
+        return new Pentatonic(path)
       }
     })
+  )
   if (foundNewPentatonic) {
     scale.edges.forEach(edge => {
-      generatePentatonics(edge.scale)
+      generatePentatonics(edge.toScale)
     })
   }
 }
@@ -204,7 +275,10 @@ function matchCycle(
   cycle: Array<Direction>
 ): boolean {
   for (let i = 0; i < edgeCycle.length; i++) {
-    if (edgeCycle[i] !== undefined && edgeCycle[i] !== cycle[i]) {
+    if (edgeCycle[i] === undefined) {
+      continue
+    }
+    if (edgeCycle[i] !== cycle[i]) {
       return false
     }
   }
@@ -249,7 +323,54 @@ function findCycle(edgeCycle: Array<Direction>): Array<Direction> {
 }
 
 function generateLayout(scale: Scale) {
-  scale.pentatonics.forEach(pentatonic => {
+  // You can't just assign directions of edges all willy-nilly, otherwise you
+  // have an edge-coloring problem on your hands. However, we know that there's
+  // an equator of scales that are all connected to each other. Let's start by
+  // finding that equator.
+  const equators = permute([scale], (path, edge) => {
+    if (path.length === 13) {
+      return false
+    }
+    if (path.length === 1) {
+      return true
+    }
+    if (path.length === 2) {
+      return edge.toScale.id !== _.first(path).id
+    }
+    if (path.length >= 3) {
+      // Every 3 must share a pentatonic
+      const pentatonic = Scale.getCommonPentatonic(path.slice(-3))
+      if (!pentatonic) {
+        return false
+      }
+      // The fourth should not share that same pentatonic otherwise its a loop
+      if (edge.toScale.isInPentatonic(pentatonic)) {
+        return false
+      }
+      return true
+    }
+  }).filter(path => _.first(path).id === _.last(path).id)
+  // There will be two results, which are the reverse of each other
+  const equator = _.initial(equators[0])
+
+  // Now we can iterate through all the pentatonic scales and assign edge
+  // directions to all of their scales. However, the order in which we do this
+  // is important. If we start with all of the pentatonics that lie along the
+  // equator, then we don't have any clusterfuck issues.
+  let pentatonics: Array<Pentatonic> = []
+  const path = equator.concat(equator.slice(0, 2))
+  for (let i = 0; i < equator.length; i++) {
+    pentatonics.push(Scale.getCommonPentatonic(path.slice(i, i + 3)))
+  }
+
+  // Keep track of whether a pentatonic is already pending a visit
+  const visiting: { [id: string]: boolean } = {}
+  pentatonics.forEach(pentatonic => {
+    visiting[pentatonic.id] = true
+  })
+
+  while (pentatonics.length > 0) {
+    const pentatonic = pentatonics.shift()
     let foundEmptyEdge = false
     const edgePairs = pentatonic.getEdgePairs()
     // We want to preserve the cycle directions that are already written
@@ -272,9 +393,16 @@ function generateLayout(scale: Scale) {
       }
     })
     if (foundEmptyEdge) {
-      pentatonic.scales.map(generateLayout)
+      const more = pentatonic.scales
+        .map(scale => scale.pentatonics)
+        .reduce((acc, list) => acc.concat(list), [])
+        .filter(pentatonic => !visiting[pentatonic.id])
+      more.forEach(pentatonic => {
+        visiting[pentatonic.id] = true
+      })
+      pentatonics = pentatonics.concat(more)
     }
-  })
+  }
 }
 
 export function createKeyWheelGraph(pegs: Pegs) {
