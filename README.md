59832d570398b0fa6fcd12bae843c406835197b1
Chet Corcos <ccorcos@gmail.com>
2017-06-25 18:19:58 -0700

    Readme demo and source link

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Key Wheel
+# Key Wheel [[Demo](https://ccorcos.github.io/key-wheel/)]
 
 This is a visualization of my friend Sean O'Reilly's math paper about music and group theory. It shows the relationship between various scales based on single-note half-step substitutions.
--- src/index.tsx
+++ src/index.tsx
@@ -8,25 +8,40 @@ import Component from "reactive-magic/component"
 class App extends Component<{}> {
   view() {
     return (
-      <div
-        style={{
-          position: "relative",
-          width: 735,
-          height: 600,
-          marginTop: 100,
-          marginLeft: "auto",
-          marginRight: "auto",
-        }}
-      >
-        <div style={{ position: "absolute", top: 0, left: 0 }}>
-          <KeyWheel />
+      <div>
+        <div
+          style={{
+            position: "relative",
+            width: 735,
+            height: 600,
+            marginTop: 100,
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        >
+          <div style={{ position: "absolute", top: 0, left: 0 }}>
+            <KeyWheel />
+          </div>
+          <div style={{ position: "absolute", top: 250, left: 50 }}>
+            <Info />
+          </div>
+          <div style={{ position: "absolute", top: 250, left: 400 }}>
+            <ScalePegs />
+          </div>
+
         </div>
-        <div style={{ position: "absolute", top: 250, left: 50 }}>
-          <Info />
-        </div>
-        <div style={{ position: "absolute", top: 250, left: 400 }}>
-          <ScalePegs />
-        </div>
+        <a
+          href="https://github.com/ccorcos/key-wheel"
+          target="_blank"
+          style={{
+            position: "absolute",
+            top: 10,
+            right: 20,
+            fontFamily: "monospace",
+          }}
+        >
+          source
+        </a>
       </div>
     )
   }
