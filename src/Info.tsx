213549bdfa902f45822d4dea8a64c5c609b3bfda
Chet Corcos <ccorcos@gmail.com>
2017-11-23 05:29:01 -0800

    Better layout

--- src/Info.tsx
+++ src/Info.tsx
@@ -7,51 +7,51 @@ import { Edge, Direction } from "./utils"
 interface InfoProps {}
 
 const edgeInfo = (edge: Edge) => {
-  return (
-    <div key={edge.id} style={{ paddingLeft: 10, paddingTop: 10 }}>
-      <div>{`peg: ${World.pegColors[edge.peg]}`}</div>
-      <div>{`spin: ${edge.spin}`}</div>
-      <div>{`direction: ${Direction[edge.direction]}`}</div>
-      <div>{`scale: ${edge.toScale.id} (${parseInt(edge.toScale.id, 2)})`}</div>
-    </div>
-  )
+	return (
+		<div key={edge.id} style={{ paddingLeft: 10, paddingTop: 10 }}>
+			<div>{`peg: ${World.pegColors[edge.peg]}`}</div>
+			<div>{`spin: ${edge.spin}`}</div>
+			<div>{`direction: ${Direction[edge.direction]}`}</div>
+			<div>{`scale: ${edge.toScale.id} (${parseInt(edge.toScale.id, 2)})`}</div>
+		</div>
+	)
 }
 
 class Info extends Component<InfoProps> {
-  view() {
-    const scale = World.hoveredScale.get()
-    const edge = World.hoveredEdge.get()
-    const style: React.CSSProperties = {
-      fontFamily: "monospace",
-    }
-    if (scale) {
-      return (
-        <div style={style}>
-          <div>{`scale: ${scale.id} (${parseInt(scale.id, 2)})`}</div>
-          <div>edges: {scale.edges.map(edgeInfo)}</div>
-        </div>
-      )
-    }
-    if (edge) {
-      return (
-        <div style={style}>
-          <div>{`edge: ${edge.id}`}</div>
-          <div>{`from: ${edge.fromScale.id} (${parseInt(
-            edge.fromScale.id,
-            2
-          )})`}</div>
-          <div>{`to: ${edge.toScale.id} (${parseInt(
-            edge.toScale.id,
-            2
-          )})`}</div>
-          <div>{`spin: ${edge.spin}`}</div>
-          <div>{`direction: ${Direction[edge.direction]}`}</div>
-          <div>{`peg: ${World.pegColors[edge.peg]}`}</div>
-        </div>
-      )
-    }
-    return null
-  }
+	view() {
+		const scale = World.hoveredScale.get()
+		const edge = World.hoveredEdge.get()
+		const style: React.CSSProperties = {
+			fontFamily: "monospace",
+		}
+		if (scale) {
+			return (
+				<div style={style}>
+					<div>{`scale: ${scale.id} (${parseInt(scale.id, 2)})`}</div>
+					<div>edges: {scale.edges.map(edgeInfo)}</div>
+				</div>
+			)
+		}
+		if (edge) {
+			return (
+				<div style={style}>
+					<div>{`edge: ${edge.id}`}</div>
+					<div>{`from: ${edge.fromScale.id} (${parseInt(
+						edge.fromScale.id,
+						2
+					)})`}</div>
+					<div>{`to: ${edge.toScale.id} (${parseInt(
+						edge.toScale.id,
+						2
+					)})`}</div>
+					<div>{`spin: ${edge.spin}`}</div>
+					<div>{`direction: ${Direction[edge.direction]}`}</div>
+					<div>{`peg: ${World.pegColors[edge.peg]}`}</div>
+				</div>
+			)
+		}
+		return null
+	}
 }
 
 export default Info
--- src/index.tsx
+++ src/index.tsx
@@ -6,45 +6,52 @@ import Info from "./Info"
 import Component from "reactive-magic/component"
 
 class App extends Component<{}> {
-  view() {
-    return (
-      <div>
-        <div
-          style={{
-            position: "relative",
-            width: 735,
-            height: 600,
-            marginTop: 100,
-            marginLeft: "auto",
-            marginRight: "auto",
-          }}
-        >
-          <div style={{ position: "absolute", top: 0, left: 0 }}>
-            <KeyWheel />
-          </div>
-          <div style={{ position: "absolute", top: 250, left: 50 }}>
-            <Info />
-          </div>
-          <div style={{ position: "absolute", top: 250, left: 400 }}>
-            <ScalePegs />
-          </div>
-
-        </div>
-        <a
-          href="https://github.com/ccorcos/key-wheel"
-          target="_blank"
-          style={{
-            position: "absolute",
-            top: 10,
-            right: 20,
-            fontFamily: "monospace",
-          }}
-        >
-          source
-        </a>
-      </div>
-    )
-  }
+	view() {
+		return (
+			<div>
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						marginTop: 20,
+					}}
+				>
+					<div
+						style={{
+							position: "relative",
+							width: 233,
+							height: 233,
+							margin: 10,
+						}}
+					>
+						<ScalePegs />
+					</div>
+					<div
+						style={{
+							position: "relative",
+							width: 733,
+							margin: 10,
+							marginTop: 28,
+						}}
+					>
+						<KeyWheel />
+					</div>
+				</div>
+				<a
+					href="https://github.com/ccorcos/key-wheel"
+					target="_blank"
+					style={{
+						position: "absolute",
+						top: 10,
+						right: 20,
+						fontFamily: "monospace",
+					}}
+				>
+					source
+				</a>
+			</div>
+		)
+	}
 }
 
 const root = document.createElement("div")
